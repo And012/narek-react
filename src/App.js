5eec6554cce25f06fb01379192d9601ce64d18aa
Andranik <andtsatur1296@gmail.com>
2024-11-23 10:26:33 +0400

    use state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
 import "./App.css";
-import { FilterableProductTable } from "./thinking-in-react";
 
 function App() {
-  return <FilterableProductTable />;
+  const [count, setCount] = useState(0);
+  console.log("it is working");
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setTimeout(() => {
+            setCount(count + 1);
+          }, 1000);
+        }}
+      >
+        {count}
+      </button>
+      <button
+        onClick={() => {
+          setTimeout(() => {
+            setCount(count + 1);
+          }, 500);
+        }}
+      >
+        {count}
+      </button>
+    </div>
+  );
 }
 export default App;
