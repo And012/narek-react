34f313254ef07e2a92129a2bb22af463e550ca90
Andranik <andtsatur1296@gmail.com>
2024-11-27 10:30:22 +0400

    todo lab first steps

--- src/todo/index.jsx
+++ src/todo/index.jsx
@@ -1,3 +1,91 @@
+import { useCallback, useState } from "react";
+import "./index.css";
+
+function Item({ todo: { completed, text }, setTodos, index }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  return (
+    <div className="list-item">
+      <div className="delete-container">
+        <button
+          className="delete"
+          onClick={() =>
+            // delete todo
+            setTodos((todos) => todos.filter((_, i) => i !== index))
+          }
+        >
+          X
+        </button>
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={() =>
+            setTodos(
+              (
+                todos // mark it as completed
+              ) =>
+                todos.map((todo, i) => {
+                  if (i === index) {
+                    return { ...todo, completed: !completed };
+                  }
+                  return todo;
+                })
+            )
+          }
+        />
+        {isEditing ? (
+          <input
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                setIsEditing(false);
+              }
+            }}
+          />
+        ) : (
+          <button className="edit" onClick={() => setIsEditing(!isEditing)}>
+            Edit
+          </button>
+        )}
+      </div>
+      <span>{text}</span>
+    </div>
+  );
+}
+
 export default function Todo() {
-  return <div>todo</div>;
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const setter = useCallback(() => {
+    // add todo
+    setTodos((prevValue) => [
+      ...prevValue,
+      { completed: false, text: inputValue },
+    ]);
+    setInputValue("");
+  }, [setTodos, setInputValue, inputValue]);
+
+  return (
+    <div className="container">
+      <div className="control">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(event) => {
+            setInputValue(event.target.value);
+          }}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              setter();
+            }
+          }}
+        />
+        <button onClick={setter}>Add todo</button>
+      </div>
+
+      {todos.map((todo, index) => (
+        <Item key={index} todo={todo} index={index} setTodos={setTodos} />
+      ))}
+    </div>
+  );
 }
