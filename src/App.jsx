1db77ce2029967f5eef24405994fc4962f403c62
Andranik <andtsatur1296@gmail.com>
2024-12-02 10:31:10 +0400

    added profiler to measure the performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Profiler } from "react";
 import "./App.css";
 import Todo from "./todo";
 import Counter from "./useReducer";
@@ -5,11 +6,16 @@ import TodosApp from "./useSyncExternalStore";
 
 function App() {
   return (
-    <>
+    <Profiler
+      id="first-profiler"
+      onRender={(...args) => {
+        console.log(args);
+      }}
+    >
       <Todo />
       <Counter />
       <TodosApp />
-    </>
+    </Profiler>
   );
 }
 export default App;
